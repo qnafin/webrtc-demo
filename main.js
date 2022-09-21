33788dd2cdd1ba141d1f9192c4626fe03c91da0c
ri_kunafin <qnafin@gmail.com>
2022-09-21 14:42:54 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+const restartConfig = {
+    iceServers: [{
+      urls: "turn:asia.myturnserver.net",
+      username: "allie@oopcode.com",
+      credential: "topsecretpassword"
+    }]
+  };
+
+let peerConnection = new RTCPeerConnection();
+
+peerConnection.setConfiguration(restartConfig)
+let localStream;
+let remoteStream;
+
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    remoteStream = new MediaStream()
+    document.getElementById('user-1').srcObject = localStream
+    document.getElementById('user-2').srcObject = remoteStream
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+        });
+    };
+}
+
+let createOffer = async () => {
+
+
+    peerConnection.onicecandidate = async (event) => {
+        //Event that fires off when a new offer ICE candidate is created
+        if(event.candidate){
+            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
+        }
+    };
+
+    const offer = await peerConnection.createOffer({ "iceRestart": true });
+    await peerConnection.setLocalDescription(offer);
+}
+
+let createAnswer = async () => {
+
+    let offer = JSON.parse(document.getElementById('offer-sdp').value)
+
+    peerConnection.onicecandidate = async (event) => {
+        //Event that fires off when a new answer ICE candidate is created
+        if(event.candidate){
+            console.log('Adding answer candidate...:', event.candidate)
+            document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription)
+        }
+    };
+
+    await peerConnection.setRemoteDescription(offer);
+
+    let answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer); 
+}
+
+let addAnswer = async () => {
+    console.log('Add answer triggerd')
+    let answer = JSON.parse(document.getElementById('answer-sdp').value)
+    console.log('answer:', answer)
+    if (!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer);
+    }
+}
+
+init()
+
+document.getElementById('create-offer').addEventListener('click', createOffer)
+document.getElementById('create-answer').addEventListener('click', createAnswer)
+document.getElementById('add-answer').addEventListener('click', addAnswer)
