d294fb6fb94aab4e5356c896300e85f8b774f955
ri_kunafin <qnafin@gmail.com>
2022-09-21 14:54:45 +0500

    add turn servers

--- main.js
+++ main.js
@@ -1,77 +1,95 @@
 const restartConfig = {
-    iceServers: [{
-      urls: "turn:asia.myturnserver.net",
-      username: "allie@oopcode.com",
-      credential: "topsecretpassword"
-    }]
-  };
+  iceServers: [
+    {
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      username: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      credential: "28224511:1379330808",
+    },
+    {
+      urls: "turn:numb.viagenie.ca",
+      credential: "muazkh",
+      username: "webrtc@live.com",
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=tcp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+  ],
+};
 
 let peerConnection = new RTCPeerConnection();
 
-peerConnection.setConfiguration(restartConfig)
+peerConnection.setConfiguration(restartConfig);
 let localStream;
 let remoteStream;
 
 let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
-    remoteStream = new MediaStream()
-    document.getElementById('user-1').srcObject = localStream
-    document.getElementById('user-2').srcObject = remoteStream
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  remoteStream = new MediaStream();
+  document.getElementById("user-1").srcObject = localStream;
+  document.getElementById("user-2").srcObject = remoteStream;
 
-    localStream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track, localStream);
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
     });
-
-    peerConnection.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-        remoteStream.addTrack(track);
-        });
-    };
-}
+  };
+};
 
 let createOffer = async () => {
+  peerConnection.onicecandidate = async (event) => {
+    //Event that fires off when a new offer ICE candidate is created
+    if (event.candidate) {
+      document.getElementById("offer-sdp").value = JSON.stringify(
+        peerConnection.localDescription
+      );
+    }
+  };
 
-
-    peerConnection.onicecandidate = async (event) => {
-        //Event that fires off when a new offer ICE candidate is created
-        if(event.candidate){
-            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
-        }
-    };
-
-    const offer = await peerConnection.createOffer({ "iceRestart": true });
-    await peerConnection.setLocalDescription(offer);
-}
+  const offer = await peerConnection.createOffer({ iceRestart: true });
+  await peerConnection.setLocalDescription(offer);
+};
 
 let createAnswer = async () => {
+  let offer = JSON.parse(document.getElementById("offer-sdp").value);
 
-    let offer = JSON.parse(document.getElementById('offer-sdp').value)
+  peerConnection.onicecandidate = async (event) => {
+    //Event that fires off when a new answer ICE candidate is created
+    if (event.candidate) {
+      console.log("Adding answer candidate...:", event.candidate);
+      document.getElementById("answer-sdp").value = JSON.stringify(
+        peerConnection.localDescription
+      );
+    }
+  };
 
-    peerConnection.onicecandidate = async (event) => {
-        //Event that fires off when a new answer ICE candidate is created
-        if(event.candidate){
-            console.log('Adding answer candidate...:', event.candidate)
-            document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription)
-        }
-    };
+  await peerConnection.setRemoteDescription(offer);
 
-    await peerConnection.setRemoteDescription(offer);
-
-    let answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer); 
-}
+  let answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+};
 
 let addAnswer = async () => {
-    console.log('Add answer triggerd')
-    let answer = JSON.parse(document.getElementById('answer-sdp').value)
-    console.log('answer:', answer)
-    if (!peerConnection.currentRemoteDescription){
-        peerConnection.setRemoteDescription(answer);
-    }
-}
+  console.log("Add answer triggerd");
+  let answer = JSON.parse(document.getElementById("answer-sdp").value);
+  console.log("answer:", answer);
+  if (!peerConnection.currentRemoteDescription) {
+    peerConnection.setRemoteDescription(answer);
+  }
+};
 
-init()
+init();
 
-document.getElementById('create-offer').addEventListener('click', createOffer)
-document.getElementById('create-answer').addEventListener('click', createAnswer)
-document.getElementById('add-answer').addEventListener('click', addAnswer)
+document.getElementById("create-offer").addEventListener("click", createOffer);
+document
+  .getElementById("create-answer")
+  .addEventListener("click", createAnswer);
+document.getElementById("add-answer").addEventListener("click", addAnswer);
